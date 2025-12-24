644db8076f37198f9e969dd35612c17d831dc85b
subin <subin940113@gmail.com>
2025-12-24 10:56:03 +0900

    feat : 비동기 처리 활성화 설정 추가

---
+++ src/main/kotlin/com/example/lifelog/common/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package com.example.lifelog.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfig
